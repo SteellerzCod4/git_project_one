a032118d53a95ac9cb103f30347667d9dc8360d3
SteellerzCod4 <83817014+SteellerzCod4@users.noreply.github.com>
2025-04-02 18:52:05 +0300

    add about handler

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "hi, chel"
 
+@app.route("/about")
+def about():
+    return "Flask #1: Что это такое? Простое WSGI-приложение"
+
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
